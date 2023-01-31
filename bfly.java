9c62c378678c024bbaff81b3db4f003976f17016
maheshmahi21 <124039471+maheshmahi21@users.noreply.github.com>
2023-01-31 15:15:22 +0530

    Add files via upload

---
+++ bfly.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class bfly {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j =1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            int sp = 2 * (n-i);
+            for(int j =1; j<=sp; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+   }
+//        System.out.println();
+        for(int i = n; i>=0; i--)
+        {
+            for(int j =1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            int sp = 2 * (n-i);
+            for(int j =1; j<=sp; j++)
+            {
+                System.out.print("  ");
+            }
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
